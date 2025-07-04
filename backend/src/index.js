c895d3559b78ece36d86e97869ba8b86252f8e76
Saggywet <117952512+Saggywet@users.noreply.github.com>
2025-07-03 22:13:16 -0400

    Connected MongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,15 @@
 import express from "express"
 import dotenv from "dotenv"
+
+import { connectDB } from "./lib/db.js"
+
 import adminRoutes from "./routes/admin.route.js"
 import albumRoutes from "./routes/album.route.js"
 import authRoutes from "./routes/auth.route.js"
 import songRoutes from "./routes/song.route.js"
 import statsRoutes from "./routes/stats.route.js"
 import userRoutes from "./routes/user.route.js"
+
 
 
 
@@ -24,4 +28,5 @@ app.use("/api/stats", statsRoutes)
 
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
+    connectDB();
 })
