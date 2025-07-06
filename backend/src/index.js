26e3f14a02b3264ad10f46154d6a9f73280be5d4
Saggywet <117952512+Saggywet@users.noreply.github.com>
2025-07-06 17:25:31 -0400

    Added Fileupload feature if user is an admin

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,18 +10,28 @@ import authRoutes from "./routes/auth.route.js"
 import songRoutes from "./routes/song.route.js"
 import statsRoutes from "./routes/stats.route.js"
 import userRoutes from "./routes/user.route.js"
-
+import fileUpload from "express-fileupload"
+import path from "path"
 
 
 
 dotenv.config();
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-
 app.use(clerkMiddleware({path: '/api/auth'})); //this will add 'auth' to required object
+app.use(fileUpload({
+    useTempFiles:true,
+    tempFileDir: path.join(__dirname, "tmp"),
+    createParentPath: true,
+    limits: {
+        fileSize: 10 * 1024 * 1024, //10MB max file size
+    },
+})
+);
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes)
