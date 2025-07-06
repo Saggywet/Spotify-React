16024821503fd2ec72c5c3960f0fe8b99a6ac9e2
Saggywet <117952512+Saggywet@users.noreply.github.com>
2025-07-06 17:06:01 -0400

    Added the feature to check if the user is logged in as an 'admin'

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import { clerkMiddleware } from '@clerk/express'
 
 import { connectDB } from "./lib/db.js"
 
@@ -19,6 +20,8 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+
+app.use(clerkMiddleware({path: '/api/auth'})); //this will add 'auth' to required object
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes)
