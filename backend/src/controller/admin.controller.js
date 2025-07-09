2dc5ce505d3610f8b802146ceff62b22bd576892
Saggywet <117952512+Saggywet@users.noreply.github.com>
2025-07-08 21:08:29 -0400

    Added 'Checkadmin' for certain commands

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -116,3 +116,7 @@ export const deleteAlbum = async (req, res, next) => {
 
     }
 }
+
+export const checkAdmin = async (req, res, next) => {
+    res.status(200).json({ admin: true });
+}
--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,13 +1,18 @@
 import { Router } from "express";
-import { createSong, deleteSong, createAlbum, deleteAlbum} from "../controller/admin.controller.js";
+import { createSong, deleteSong, createAlbum, deleteAlbum, checkAdmin} from "../controller/admin.controller.js";
 import { protectRoute, requireAdmin } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.post("/songs", protectRoute,requireAdmin, createSong);
-router.post("/albums", protectRoute,requireAdmin, createAlbum);
-router.delete("/songs/:id", protectRoute, requireAdmin, deleteSong);
-router.delete("/albums/:id", protectRoute, requireAdmin, deleteAlbum);
+router.use(protectRoute, requireAdmin) //allows for the other routers access to these functions
+
+router.get("/check", checkAdmin)
+
+router.post("/songs", createSong);
+router.post("/albums", createAlbum);
+
+router.delete("/songs/:id", deleteSong);
+router.delete("/albums/:id", deleteAlbum);
 
 
 export default router;
